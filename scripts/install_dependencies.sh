a1143a13761278c325696dfd71e96b480e04e3d1
Enock Momanyi <enockenm@gmail.com>
2023-08-08 15:39:18 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,6 +5,7 @@ sudo amazon-linux-extras enable php8.0
 yum install -y php-cli php-pdo php-fpm php-mysqlnd
 
 sed -i "s#user.*=.*apache#user = nginx#" /etc/php-fpm.d/www.conf
+sed -i "s#listen.acl_user.*#listen.acl_users = nginx#" /etc/php-fpm.d/www.conf
 sed -i "s#group.*=.*apache#group = nginx#" /etc/php-fpm.d/www.conf
 sed -i "s#listen.*www.sock#listen = /var/run/php-fpm/www.sock#" /etc/php-fpm.d/www.conf
 sed -i "s#.*listen.owner.*#listen.owner = nginx#" /etc/php-fpm.d/www.conf
@@ -18,7 +19,7 @@ systemctl start php-fpm
 
 sudo amazon-linux-extras install -y nginx1
 
-sed -i "s#root.*#root       /var/www/html;#" /etc/nginx/nginx.conf
+sed -i "s#root.*#root        /var/www/html;#" /etc/nginx/nginx.conf
 
 systemctl restart php-fpm
 systemctl restart nginx
